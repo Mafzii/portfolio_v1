555ac4fd12063a41b994c0766ca72803cb9b5df1
Mafzii <23100040@lums.edu.pk>
2023-09-30 20:57:47 +0500

    DEV-4: Projects | cleaned up code and set up on windows env

--- components/About.jsx
+++ components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <h1 className="text-black text-3xl tracking-tight">about...</h1>
             <div className="grid grid-cols-6">
                 <p className="bg-primary text-background drop-shadow-lg rounded-lg p-4 col-span-6 my-4">
-                    On a mission to build meaningful solutions. I love tackling complex challenges, working with passionate teams, and staying curious about the ever-changing tech world.
+                    On a mission to build meaningful solutions. I love tackling complex challenges, working with passionate teams, and learning about the ever-changing tech world.
                     <br /><br />
                     As a recent graduate I am excited to apply myself and work on some cool projects with a diverse range of tech.
                 </p>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -17,64 +17,15 @@ const _projects = [
 ];
 
 export default function Projects() {
-    // make a usestate for all 4 projects
     const [project1, setProject1] = useState(false);
-    const project2 = useState(false);
-    const project3 = useState(false);
-    const project4 = useState(false);
+    const [project2, setProject2] = useState(false);
+    const [project3, setProject3] = useState(false);
+    const [project4, setProject4] = useState(false);
 
     return (
         <section id="projects" className="my-4 drop-shadow-lg">
             <h1 className="text-black text-3xl tracking-tight">projects...</h1>
             <div className="my-4 grid grid-flow-row grid-cols-2 gap-2">
-                {/* make animation so that green title expands upwards to fill the whole card and information is shown there:
-                on default it should show title and a small gif of the project
-                */}
-                {
-                    // _projects.map((project, index) => {
-                    //     return (
-                    //         <motion.div
-                    //             key={'project' + index}
-                    //             layoutId={'project' + index}
-                    //             onClick={() => setSelectedId('project' + index)}
-                    //             className="relative bg-background-dark rounded-lg hover:drop-shadow-[0_10px_5px_rgba(0,0,0,0.25)] w-full h-[16rem]">
-                    //             <AnimatePresence>
-                    //                 {selectedId && (
-                    //                     <motion.div
-                    //                         key={'modal' + selectedId}
-                    //                         layoutId={selectedId}
-                    //                         onClick={() => setSelectedId(null)}
-                    //                         className="fixed w-1/2 h-[16.25rem] bg-transparent"
-                    //                         initial={{ scale: 0 }}
-                    //                         animate={{ scale: 2 }}
-                    //                         exit={{ scale: 0 }}>
-                    //                         <motion.div className="h-full w-full bg-primary rounded-lg text-white bottom-0">
-                    //                             <div className="h-full text-2xl flex justify-start p-2 px-4">
-                    //                                 <h1 className="text-sm">pp</h1>
-                    //                             </div>
-
-                    //                         </motion.div>
-                    //                     </motion.div>
-                    //                 )}
-                    //             </AnimatePresence>
-                    //                 {!selectedId && (
-                    //                     <motion.div>
-                    //                         <motion.div className="bg-background-dark rounded-lg">
-                    //                             {/* main container before animation */}
-                    //                         </motion.div>
-                    //                         <motion.div className="absolute w-full bg-primary rounded-lg text-white bottom-0">
-                    //                             <div className="h-full text-2xl flex justify-start p-2 px-4">
-                    //                                 <h1 className="text-sm">{project["name"]}</h1>
-                    //                             </div>
-
-                    //                         </motion.div>
-                    //                     </motion.div>
-                    //                 )}
-                    //         </motion.div>
-                    //     )
-                    // })
-                }
-                {/* make the same as above but without the map */}
                 <motion.div
                     layoutId="project1_out"
                     onClick={() => setProject1(!project1)}
@@ -82,7 +33,10 @@ export default function Projects() {
                     {!project1 && (
                         <motion.div className="">
                             <motion.div className="bg-background-dark rounded-lg">
-                                {/* main container before animation */}
+                                
+                            </motion.div>
+                            <motion.div className="fixed bottom-0 bg-primary rounded-lg h-[2rem] text-white">
+                                Play On
                             </motion.div>
                         </motion.div>
                     )}
