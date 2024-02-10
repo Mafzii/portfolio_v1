ea01aed7da003b4d7f0942151d6c4d064730b9d4
Mafzii <23100040@lums.edu.pk>
2024-02-11 02:48:27 +0500

    Created new projects section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="h-[8rem] bg-background"></div>;
+  return <div className="mt-4 h-[8rem] bg-primary"></div>;
 }
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,27 +4,40 @@ import Image from "next/image";
 
 const _projects = [
   {
-    name: "flow fields",
-  },
-  {
-    name: "HITCHERR",
-  },
-  {
-    name: "MOOLA",
-  },
-  {
-    name: "SEQUENCE",
+    name: "flow system",
+    image: "/image/flow-fields.gif",
+    description:
+      "Creative coding project stemming from an interest in creating a randomized particle system",
   },
 ];
 
-
-
 export default function Projects() {
   return (
-    <section id="projects" className="my-4 drop-shadow-lg">
+    <section id="projects" className="my-4">
       <h1 className="text-black text-3xl tracking-tight">projects...</h1>
-      <div className="my-4 grid grid-flow-row grid-cols-2 gap-2">
-        
+      <div className="my-4">
+        {_projects.map((project, index) => (
+          <div className="my-6 grid grid-flow-row grid-cols-2 gap-6">
+            <div className="w-full bg-background-dark rounded-md p-4 drop-shadow-lg">
+              <Image
+                src={project.image}
+                alt="Gif of the project"
+                format="gif"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-md p-4"
+              />
+            </div>
+            <div className="grid grid-flow-row grid-rows-2">
+              <div className="flex items-end">
+                <h1 className="font-light tracking-tight text-3xl">
+                  {project.name}
+                </h1>
+              </div>
+              <div className="bg-primary text-background rounded-md p-4 drop-shadow-lg">{project.description}</div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
